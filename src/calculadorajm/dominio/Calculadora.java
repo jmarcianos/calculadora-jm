7aba5d51b31f8041dbfbc73a9611bcdd8056e044
jmarcianos <josemarciano@tutanota.com>
2022-06-20 14:53:30 -0300

    commit inicial

--- src/calculadorajm/dominio/Calculadora.java
+++ src/calculadorajm/dominio/Calculadora.java
@@ -1,5 +1,24 @@
 package calculadorajm.dominio;
 
 public class Calculadora {
-    private String nome;
+    public double soma(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtracao(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiplicacao(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double divisao(double num1, double num2) {
+        if (num2 == 0.0) {
+            //System.out.println("Divisão inválida, não se pode dividir por 0");
+            return 0;
+        }
+        return num1 / num2;
+    }
+
 }
